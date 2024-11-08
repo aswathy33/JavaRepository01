508e97bd43482ac90eb38eddcb6e308564179df7
Sidhi <Sidhi@LAPTOP-C07NBRNB>
2024-11-08 19:38:41 +0530

    Sample program commit

---
+++ javaprogram1/src/sample/javaprogram/PrintNumbers.java
@@ -0,0 +1,17 @@
+package sample.javaprogram;
+
+public class PrintNumbers {
+
+	public void printNum() {
+	for(int i=0;i<=10;i++)	
+	{
+		System.out.println(i);
+	}
+	}
+	public static void main(String[] args) {
+		PrintNumbers obj= new PrintNumbers();
+		obj.printNum();
+
+	}
+
+}
---
+++ javaprogram1/src/sample/javaprogram/SampleProgram1.java
@@ -0,0 +1,15 @@
+package sample.javaprogram;
+
+public class SampleProgram1 {
+
+	public void printWord() {
+		
+	System.out.println("Hello World")	;
+	}
+	
+	public static void main(String[] args) {
+		SampleProgram1 obj=new SampleProgram1();
+		 obj.printWord();
+	}
+
+}
