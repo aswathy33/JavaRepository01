d33258a278d5f8392e5afac26084eaae4548e6a1
Sidhi <Sidhi@LAPTOP-C07NBRNB>
2024-11-27 17:52:50 +0530

    sample program

---
+++ javaprogrammingjenkins/src/main/java/javaprogrammingjenkins/SimpleProgram.java
@@ -0,0 +1,15 @@
+package javaprogrammingjenkins;
+
+public class SimpleProgram {
+
+	public void print()
+	{
+		System.out.println("Hello World");
+	}
+	public static void main(String[] args) {
+		SimpleProgram obj=new SimpleProgram();
+		obj.print();
+
+	}
+
+}
